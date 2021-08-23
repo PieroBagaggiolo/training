eb6cd08d7f5dbb9b3dcac6ddf875ffe51bd2b8bd
Riccardo Barberini <barberini.riccardo@gmail.com>
2021-08-23 14:37:58 +0200

    Modifica js

--- training-git/js/script.js
+++ training-git/js/script.js
@@ -1,5 +1,9 @@
-let body = document.querySelector("body");
-
-body.addEventListener("click", function () {
-  this.classList.toggle = "green";
-});
+window.onload = function () {
+  // let body = document.body;
+  let h3 = document.querySelector("#target");
+  h3.classList.add = "green";
+  console.log(h3);
+  // console.log(body);
+  /* body.addEventListener("click", function () {
+}); */
+};
